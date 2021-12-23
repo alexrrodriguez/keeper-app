3915f5a6007044de8ef90c2d1e960c2c056bbbd4
alex <arodriguez202020@gmail.com>
2021-12-23 10:45:59 -0600

    create CreateArea component and import to App.jsx to render in App component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,16 +2,18 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
-import notes from "../notes";
+// import notes from "../notes";
+import CreateArea from "./CreateArea";
 
 function App() {
   return (
     <div>
       <Header />
-      {notes.map((note) => (
+      <CreateArea />
+      {/* {notes.map((note) => (
         <Note key={note.id} title={note.title} content={note.content} />
-      ))}
-
+      ))} */}
+      <Note key={1} title="Note title" content="Note content" />
       <Footer />
     </div>
   );
